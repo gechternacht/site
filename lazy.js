4ae80c41d9ea8c5014a755dcbd6b8fab99ac7f6d
gechternacht-vinci <gabriel@agenciavinci.com.br>
2020-08-31 17:30:44 -0300

    Lazy loading

---
+++ lazy.js
@@ -0,0 +1,58 @@
+// Gets template content and clone it into target
+
+function useTemplate(templateParent,templateMediaSize,callback){
+    templateParent.appendChild(
+    templateParent.querySelector(templateMediaSize)
+    .content.cloneNode(true))
+
+    return callback();
+}
+
+
+function lazyLoad(templateParent,templateMediaSize){
+
+    // Initiate a new IO object
+    const lazyLoadIO = new IntersectionObserver((e) =>  {
+      let i = 0;
+      e.forEach( (e) => {
+        console.log(e.intersectionRatio);
+        if( e.intersectionRatio > 0 ) {
+          // Unobserve
+          lazyLoadIO.unobserve(e.target);
+            useTemplate(e.target,templateMediaSize);
+        }
+      });
+    }, {
+      // Use positive margin to load the images before they come into view
+      rootMargin: "0px 0px 150px 0px", 
+      //threshold: [0, 0.25, 0.5, 0.75]
+    });
+
+    // Loop through all figures
+    let figures = document.querySelectorAll(templateParent);
+    if(figures.length) {
+      figures.forEach( (e) => {
+        lazyLoadIO.observe(e);
+      });
+    }
+
+};
+
+// Calling functions
+
+  //lazy images 
+    lazyLoad('figure.lazy','template.desktop');
+
+  // fullbanner 
+    useTemplate(document.querySelector('div.fullbanner'),'template.desktop',
+      function(){
+        $('.fullbanner').slick({
+          dots: true,
+          slide: 'img',
+          infinite: true,
+          speed: 500,
+          fade: true,
+          cssEase: 'linear'
+        });
+    });
+
